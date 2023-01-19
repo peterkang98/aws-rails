80f1b44e2c380748cd75f9cdeff0d879bfe59894
peter <peterkang98@gmail.com>
2023-01-19 15:54:17 +0900

    signup and session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'signup', to:'users#new'
+  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to:'users#new'
   get '/about', to: "pages#about"
   root "pages#home"
 
